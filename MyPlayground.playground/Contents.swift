d6f4ac9061101aa94c86c89a92e3aba098b8ebbd
K1pore <108589318+K1pore@users.noreply.github.com>
2022-07-03 13:40:18 +0500

    task correct

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -2,4 +2,33 @@ import UIKit
 
 //HomeWork
 
+/*
+ Задание 8
 
+ Напишите функцию, которая повторяет заданную строку N раз.
+ Функция принимает на вход значение типа String (строку для повторений) и значение типа Int (количество повторений) и возвращает полученный результат.
+ 
+ Пример:
+ funcName("Swift", 2) -> "SwiftSwift"
+ funcName("Xcode", 0) -> ""
+ */
+
+// --
+
+func srtReapit(to phrase: String, in count: Int) -> String {
+
+    var result = ""
+    
+    for _ in 0..<count {
+        result += phrase
+    }
+    return result
+}
+
+let someText = "text"
+var someNumber = 4
+var start = 0
+for _ in start..<someNumber {
+    print(someText)
+}
+srtReapit(to: "Hi", in: 3)
